b51fd2c61f96cfe98aaa20aea691c590747e5b26
Abdullah Khokhar <abdullah.khokhar@mail.utoronto.ca>
2019-05-29 07:46:56 -0400

    completed the intro component

--- first-component.js
+++ first-component.js
@@ -12,9 +12,22 @@ class Hello extends React.Component{
         <h1> Hello There! </h1>
         <h1> Hello There! </h1>
       </div>
-      // so this is a single component still that renders multuple things 
+      // so this is a single component still that renders multuple things
     );
   }
+}
+
+// We can re-write the above as a function component
+function Hello(){
+  return(
+    // Remember we can only return a SINGLE element, so cannot have 3 hellos in a row, instead do
+    <div>
+      <h1> Hello There! </h1>
+      <h1> Hello There! </h1>
+      <h1> Hello There! </h1>
+    </div>
+    // so this is a single component still that renders multuple things
+  );
 }
 
 // spesify what it is we want to render and where
